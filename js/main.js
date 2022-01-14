122116e0feb48fe70614846c7a347e0c20e835ef
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2022-01-14 12:15:58 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 $(function() {
-  // AOS Animation JS
-  AOS.init({
-    easing: 'ease-in-out-sine'
-  });
-  // Jarallax JS
-  $(".jarallax").jarallax({
-    speed: 0.2
-  });
   // Hamburger Menu JS
 	$(".burger-menu").click(function() {
+    $(this).toggleClass("active");
 		$(".fixed-navbar-menu-mobile").toggleClass("active");
 		$(".menu-listing-mobile").toggleClass("active");
 		$("body").toggleClass("active");
@@ -18,5 +11,13 @@ $(function() {
     $(".menu-listing-mobile > li > a").parent().removeClass("active");
     $(this).parent().addClass("active");
     $(this).next().slideToggle();
+  });
+  // AOS Animation JS
+  AOS.init({
+    easing: 'ease-in-out-sine'
+  });
+  // Jarallax JS
+  $(".jarallax").jarallax({
+    speed: 0.2
   });
 });
